28cbdce2f161d50cc1152a34b3bf03e28886c22a
ostroushko <evgenii.ostroushko@together.com>
2020-06-29 11:36:29 +0300

    Fix warnings

--- src/js/popup.js
+++ src/js/popup.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', event => {
             document.querySelector('input[name="hiddenTop"]').checked = response.hiddenTop;
         })
 
-	document.querySelector('label[for="hiddenTop"]').innerHTML = browser.i18n.getMessage("label_top_hidden");
+	document.querySelector('label[for="hiddenTop"]').textContent = browser.i18n.getMessage("label_top_hidden");
 
 	document.querySelector('input[name="hiddenTop"]').addEventListener('change', event => {
 		browser.storage.local.set({hiddenTop: event.target.checked})
